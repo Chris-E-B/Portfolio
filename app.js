36500ac0d0ea89831db12e70cbecfbd3dd142dba
Chris <chrisbwfh@gmail.com>
2023-11-19 17:09:46 -0500

    Finished Pop up add on

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+let contact_button = document.getElementById("times");
+let contact = document.getElementById("contact_container");
+let contact_link = document.getElementById("contact_link")
 
+contact_button.addEventListener("click", () => {
+    contact.style.visibility = "hidden";
+})
 
-function output() {
-
-
-}
-output();
+contact_link.addEventListener("click", () => {
+    contact.style.visibility = "visible";
+})
