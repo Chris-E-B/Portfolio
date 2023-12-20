1f2721a667b653a5a838ce9a78fa558f95fbce01
Chris <chrisbwfh@gmail.com>
2023-12-20 16:17:59 -0500

    Updated 12/20/23

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let contact_button = document.getElementById("times");
 let contact = document.getElementById("contact_container");
-let contact_link = document.getElementById("contact_link")
+let contact_link = document.getElementById("contact_link");
+let burger = document.getElementById("burger");
+let nav = document.getElementById("nav_list");
 
 contact_button.addEventListener("click", () => {
     contact.style.visibility = "hidden";
@@ -8,4 +10,8 @@ contact_button.addEventListener("click", () => {
 
 contact_link.addEventListener("click", () => {
     contact.style.visibility = "visible";
+})
+
+burger.addEventListener("click", () => {
+    nav.style.visibility= "visible";
 })
